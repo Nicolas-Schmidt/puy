f312cab690abdd854a0b325edbe83e748c197d82
Nicolas-Schmidt <nicoschlab@gmail.com>
2020-09-28 18:56:05 -0300

    legislaturas

---
+++ R/legislaturas.R
@@ -0,0 +1,18 @@
+#' legislaturas
+#'
+#' Contiene las fechas en las que estuvieron abiertas las legislaturas desde 1830 a 2020.
+#'
+#'\describe{
+#'    \item{legislatura}{Numero de la legislatura}
+#'    \item{inicio}{Fecha de inicio de la legislatura.}
+#'    \item{fin}{Fecha de cierre de la legislatura}
+#'    \item{dias}{Cantidad de dias que estuvo abierta la legislatura.}
+#' }
+#' @docType data
+#' @keywords datasets
+#' @name legislaturas
+#' @usage data(politicos)
+#' @format data.frame con 46 filas y 4. columnas.
+#' @details La unidad de analisis de esta base de datos es...
+#'
+'legislaturas'
---
+++ data-raw/legislaturas_periodos.R
@@ -0,0 +1,25 @@
+
+
+
+library(tabulizer)
+library(tidyverse)
+file <- "C:\\Users\\usuario\\Desktop\\R_packages\\speech\\ParlamentariosUruguayos.pdf"
+
+# tabla de periodos de legislaturas
+tabla_legis <- extract_tables(file, pages = 96)
+tabla_legis <- tabla_legis[[1]]
+tabla_legis <- tabla_legis[-c(1,2,3),]
+
+all <- data.frame(
+    legislatura = tabla_legis[,1],
+    inicio_old  = tabla_legis[,3],
+    fin_old     = tabla_legis[,4]
+)
+
+all$inicio <- lubridate::parse_date_time(crr$inicio_old, order = "dmy")
+all$fin    <- lubridate::parse_date_time(crr$fin_old, order = "dmy")
+all$dias   <- lubridate::time_length(lubridate::interval(all$inicio, all$fin), "days")
+
+legislaturas <- all[, c(1, 4, 5, 6)]
+save(legislaturas, file = "data/legislaturas.rda")
+
