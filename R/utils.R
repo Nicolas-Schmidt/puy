0c316d369197535285d469001a3e50c868ab2fe7
Nicolas-Schmidt <nicoschlab@gmail.com>
2020-12-10 23:51:02 -0300

    examplehyfeovce74r6510341374613510d3

--- R/utils.R
+++ R/utils.R
@@ -5,7 +5,6 @@
 vars <- c("politicos", "legislador2", "nombre", "cantidad","party", "legislator",
           "partido", "legislator_nc", "legislature", "legislatura")
 
-
 if(getRversion() >= "2.15.1"){
     utils::globalVariables(c('.', vars))
     utils::suppressForeignCheck(c('.'))
