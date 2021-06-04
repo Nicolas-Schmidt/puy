4c7ecab35e9c60f2fd574f941e3566c9ca8bb07c
Nicolas-Schmidt <nicoschlab@gmail.com>
2021-06-04 15:16:16 -0300

    enies

--- R/as_speech_politicos.R
+++ R/as_speech_politicos.R
@@ -30,13 +30,14 @@ as_speech_politicos <- function(floor_speech){
 
 
 url_speech <- "https://parlamento.gub.uy/documentosyleyes/documentos/diario-de-sesion/senadores/2094/IMG/0?width=800&height=600&hl=en_US1&iframe=true&rel=nofollow"
-floor_speech <- speech::speech_build(file = url_speech, compiler = TRUE)
+floor_speech <- speech::speech_build(file = url, compiler = TRUE)
 
 str(floor_speech)
 
 puto <-
-l %>%
-    rename(legislator_nc = legislador2,
+politicos %>%
+    transform(legislator2 = chartr('\u00c1\u00c9\u00cd\u00d3\u00da','AEIOU', legislador)) %>%
+    rename(legislator_nc = legislator2,
            legislature   = legislatura,
            party         = partido) %>%
     transform(legislator = stringr::str_extract(legislator_nc, pattern = "[A-Z. ]{2,}")) %>%
@@ -44,10 +45,28 @@ l %>%
     merge(floor_speech, ., by = c("legislator", "legislature"), all.x = TRUE) %>%
     tibble::as_tibble()
 
+
+###### hay que eliminar duplicados de legislador si al entrar los datos no hay duplicados !!!!
+
+
 View(puto)
 
 
-floor_speech_party <- puy::as_speech_politicos(floor_speech)
 
-str(floor_speech_party)
+
+which(floor_speech$speech )
+
+
+
+## esto va para speech
+add_sex <- function(data){
+    data$sex <- ifelse(stringr::str_detect(data$speech, pattern = "^SEÑORA"), 0, 1)
+    data
+
+}
+
+
+
+floor <- add_sex(floor_speech)
+
 
