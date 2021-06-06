d7132cf59ece29fb23e7f3eda43d1e1fc1fea9c9
Nicolas-Schmidt <nicoschlab@gmail.com>
2021-06-06 00:13:47 -0300

    version 1

--- R/as_speech_politicos.R
+++ R/as_speech_politicos.R
@@ -1,72 +1,36 @@
 #' @title Adapta base de datos depliticos para pegar con speech
 #' @description Adapta base de datos depliticos para pegar con speech
-#' @param floor_speech diario de sesion en formato data.frame del paquete \code{speech}.
+#' @param speech diario de sesion en formato data.frame del paquete \code{speech}.
 #' @return data.frame
 #' @examples
 #' library(speech)
 #' url <- "http://bit.ly/35AUVF4"
-#' text <- speech::speech_build(file = url)
-#' as_speech_politicos(floor_speech = text)
+#' text <- speech::speech_build(file = url, compiler = TRUE)
+#' text <- speech::speech_legis_replace(tidy_speech = text, old = "GOI", new = "GOÑI")
+#' floor_speech <- as_speech_politicos(speech = text)
 #' @export
 
-as_speech_politicos <- function(floor_speech){
 
-    politicos %>%
-        rename(legislator_nc = legislador2,
-               legislature   = legislatura,
-               party         = partido) %>%
-        transform(legislator = stringr::str_extract(legislator_nc, pattern = "[A-Z. ]{2,}")) %>%
-        subset(., select = c(legislator_nc, legislature, party, legislator)) %>%
-        merge(floor_speech, ., by = c("legislator", "legislature")) %>%
-        tibble::as_tibble()
-}
+as_speech_politicos <- function(speech){
 
+    dat <- speech
+    # acá hay que filtrar la base de politicos solo de legisladores
+    b1          <- politicos
+    b1$apellido <- stringr::str_extract(b1$politico, pattern = "[A-Z. ]{2,}")
+    b1$camara   <- ifelse(b1$cargo == "Senado", "CAMARA DE SENADORES", ifelse(b1$cargo == "Diputados", "CAMARA DE REPRESENTANTES", NA))
+    b2          <- b1
+    b2$camara   <- "ASAMBLEA GENERAL"
+    b3          <- b2
+    b2$camara   <- "COMISION PERMANENTE"
+    B           <- rbind(b1, b2, b3)
 
-#### ALGORITMO DE MERGE #####
-# -------  step 1 puro
-# -------  step 2 primer apellido
-# -------  step 3 legislatura anterior
-
-
-
-url_speech <- "https://parlamento.gub.uy/documentosyleyes/documentos/diario-de-sesion/senadores/2094/IMG/0?width=800&height=600&hl=en_US1&iframe=true&rel=nofollow"
-floor_speech <- speech::speech_build(file = url, compiler = TRUE)
-
-str(floor_speech)
-
-puto <-
-politicos %>%
-    transform(legislator2 = chartr('\u00c1\u00c9\u00cd\u00d3\u00da','AEIOU', legislador)) %>%
-    rename(legislator_nc = legislator2,
-           legislature   = legislatura,
-           party         = partido) %>%
-    transform(legislator = stringr::str_extract(legislator_nc, pattern = "[A-Z. ]{2,}")) %>%
-    subset(., select = c(legislator_nc, legislature, party, legislator)) %>%
-    merge(floor_speech, ., by = c("legislator", "legislature"), all.x = TRUE) %>%
-    tibble::as_tibble()
-
-
-###### hay que eliminar duplicados de legislador si al entrar los datos no hay duplicados !!!!
-
-
-View(puto)
-
-
-
-
-which(floor_speech$speech )
-
-
-
-## esto va para speech
-add_sex <- function(data){
-    data$sex <- ifelse(stringr::str_detect(data$speech, pattern = "^SEÑORA"), 0, 1)
-    data
+    match1 <- list()
+    for(i in 1:6){match1[[i]] <- step(B, dat, i)[,8:9]}
+    voi <- base::Reduce(function(...) cbind(...), match1 )
+    dat$legislator2 <- apply(voi[, seq(1,12, 2)], 1, aux)
+    dat$party <- apply(voi[, seq(2,12, 2)], 1, aux)
+    dat$indicator <- apply(voi[, seq(1,12, 2)], 1, aux2)
+    invisible(dat)
 
 }
-
-
-
-floor <- add_sex(floor_speech)
-
 
--- R/utils.R
+++ R/utils.R
@@ -1,11 +1,42 @@
 #' @importFrom magrittr %>%
-#' @importFrom dplyr group_by summarise ungroup distinct summarize n rename select
 #' @importFrom tibble as_tibble
 
-vars <- c("politicos", "legislador2", "nombre", "cantidad","party", "legislator",
+vars <- c("politicos", "legislador2","party", "legislator",
           "partido", "legislator_nc", "legislature", "legislatura")
 
 if(getRversion() >= "2.15.1"){
     utils::globalVariables(c('.', vars))
     utils::suppressForeignCheck(c('.'))
 }
+
+
+step <- function(x, y, step){
+
+    if(step == 1){x$id <- paste(x$apellido,          x$legislatura,      x$camara, x$sexo)} # match completo
+    if(step == 2){x$id <- paste(surname(x$apellido), x$legislatura,      x$camara, x$sexo)} # un solo apellido
+    if(step == 3){x$id <- paste(x$apellido,          c(x$legislatura-1), x$camara, x$sexo)} # legislatura anterior
+    if(step == 4){x$id <- paste(x$apellido,          c(x$legislatura+1), x$camara, x$sexo)} # legislatura posterior
+    if(step == 5){x$id <- paste(surname(x$apellido), c(x$legislatura-1), x$camara, x$sexo)} # legislatura anterior y un solo apellido
+    if(step == 6){x$id <- paste(surname(x$apellido), c(x$legislatura+1), x$camara, x$sexo)} # legislatura posterior y un solo apellido
+    y$id <- paste(y$legislator, y$legislature, y$chamber, y$sex)
+    out <-
+        merge(y, x[, c("id", "politico", "partido")], by = "id", all.x = TRUE) %>%
+        tibble::as_tibble()
+    names(out)[7:8] <- paste(names(out)[7:8], step, sep = "_")
+    out
+}
+
+
+surname <- function(B){
+    j <- strsplit(B, " ")
+    j2 <- sapply(j, "[", 1)
+    ubic <- which(lengths(j) > 2)
+    j2[ubic] <- B[ubic]
+    j2
+
+}
+
+aux <- function(x){ifelse(!is.na(unique(x)[1]), unique(x)[1], ifelse(length(unique(x)) > 1, unique(x)[2], NA))}
+
+aux2 <- function(x){ifelse(length(unique(x)) == 1 && is.na(unique(x)), NA, which(!is.na(x))[1])}
+
