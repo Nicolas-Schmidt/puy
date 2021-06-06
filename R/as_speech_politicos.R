3228ac20ee586a5ca94da691e6ab9e6120b43333
Nicolas-Schmidt <nicoschlab@gmail.com>
2021-06-06 14:45:54 -0300

    party acron

--- R/as_speech_politicos.R
+++ R/as_speech_politicos.R
@@ -1,13 +1,15 @@
-#' @title Adapta base de datos depliticos para pegar con speech
-#' @description Adapta base de datos depliticos para pegar con speech
+#' @title Adapta base de datos de pliticos uruguayos para pegar con base de datos generada por \code{speech::speech_build()}.
+#' @description Adapta base de datos de pliticos para pegar con speech. Es relevante para tener la filiacion partidaria de los
+#'     legisladores y poder hacer analisis con el corte partidario.
 #' @param speech diario de sesion en formato data.frame del paquete \code{speech}.
-#' @return data.frame de entrada al que se le agregan 3 variables:
+#' @return data.frame de entrada al que se le agregan 4 variables:
 #'      \itemize{
 #'             \item{\code{legislator2}: Nombre completo del legislador/a}
-#'             \item{\code{party}: partido politico al que pertenece el legislador/a en ese momento}
+#'             \item{\code{party}: Partido politico al que pertenece el legislador/a en ese momento}
+#'             \item{\code{party_acron}: sigla de partido politico al que pertenece el legislador/a en ese momento}
 #'             \item{\code{indicator}: un indicador de calidad del merge entre el diario de sesion y la base de politicos. Puede asumir los siguientres valores:}
 #'                  \itemize{
-#'                  \item{1 - Match perfecto (nombre del legislador/a, legislatura, caamara y sexo)}
+#'                  \item{1 - Match perfecto (nombre del legislador/a, legislatura, camara y sexo)}
 #'                  \item{2 - Match parcial (igual al anterior pero solo con el primer apellido del legislador en la base de politicos. Sucede que mucahs veces en los diarios de sesion no aparece el legislador con ambos apellidos y en la base de politicos si.}
 #'                  \item{3 - Match parcial (igual que el valor 1 pero cambiando la identificacion de legislatura para atras en una unidad)}
 #'                  \item{4 - Match parcial (igual que el valor 1 pero cambiando la identificacion de legislatura para adelante en una unidad)}
@@ -16,6 +18,10 @@
 #'                   }
 #'
 #'            }
+#' @details Cuando se va a hacer el merge y se hacen correcciones a mano con \code{speech::speech_legis_replace()} sobre la base de speech
+#'     hay que evitar las 'enies' y los tildes. La base de polílicos cuando se exporta genera las 'enies' y en la variable \code{legislator2}
+#'     que se genera con esta funcion las enies van a aparecer pero el proceso de unios de bases no utiliza esos caracteres por lo que si decide
+#'     utilizarlos no va a ser posible ninguno de los match posibles que se describen para la variable \code{indicator}.
 #' @examples
 #' library(speech)
 #' url <- "http://bit.ly/35AUVF4"
@@ -43,7 +49,7 @@ as_speech_politicos <- function(speech){
     dat$legislator2 <- apply(voi[, seq(1,ncol(voi), 2)], 1, aux)
     dat$party       <- apply(voi[, seq(2,ncol(voi), 2)], 1, aux)
     dat$indicator   <- apply(voi[, seq(1,ncol(voi), 2)], 1, aux2)
-    invisible(dat)
+    invisible(acron(dat))
 
 }
 
--- R/utils.R
+++ R/utils.R
@@ -9,7 +9,6 @@ if(getRversion() >= "2.15.1"){
     utils::suppressForeignCheck(c('.'))
 }
 
-
 step <- function(x, y, step){
 
     if(step == 1){x$id <- paste(x$apellido,          x$legislatura,      x$camara, x$sexo)} # match completo
@@ -19,17 +18,13 @@ step <- function(x, y, step){
     if(step == 4){x$id <- paste(x$apellido,          c(x$legislatura+1), x$camara, x$sexo)} # legislatura posterior
     if(step == 5){x$id <- paste(surname(x$apellido), c(x$legislatura-1), x$camara, x$sexo)} # legislatura anterior y un solo apellido
     if(step == 6){x$id <- paste(surname(x$apellido), c(x$legislatura+1), x$camara, x$sexo)} # legislatura posterior y un solo apellido
-
-
     y$id <- paste(y$legislator, y$legislature, y$chamber, y$sex)
-
     out <-
         merge(y, x[, c("id", "politico", "partido")], by = "id", all.x = TRUE) %>%
         tibble::as_tibble()
     names(out)[7:8] <- paste(names(out)[7:8], step, sep = "_")
     out
 }
-
 
 surname <- function(B){
     j <- strsplit(B, " ")
@@ -39,10 +34,16 @@ surname <- function(B){
     j2
 
 }
-
 aux <- function(x){ifelse(!is.na(unique(x)[1]), unique(x)[1], ifelse(length(unique(x)) > 1, unique(x)[2], NA))}
-
 aux2 <- function(x){ifelse(length(unique(x)) == 1 && is.na(unique(x)), NA, which(!is.na(x))[1])}
+acron <- function(x){
+    x. <- merge(x, Boreluy::partidos_uy[, c("Partido", "Sigla")], by.x = "party",by.y = "Partido", all.x = TRUE)
+    names(x.)[11] <- "party_acron"
+    x.[, c(2:9, 1, 11, 10)] %>% tibble::as_tibble()
+    }
+
+
+
 
 
 
